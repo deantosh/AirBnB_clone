bca68ac80b259856b1acff23ccfdd6086ace3052
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-18 21:49:43 +0300

    State class added

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+Module defines `State` class -- a blueprint to create state objects.
+"""
+
+
+class State(BaseModel):
+    """defines a `State` class"""
+    name = ""
