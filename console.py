0256bb231e40791a61b63979da3ebb98e4677678
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-19 00:26:19 +0300

    console updated

--- console.py
+++ console.py
@@ -4,6 +4,11 @@ import cmd
 import sys
 from models import storage
 from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
 from models.base_model import BaseModel
 
 """
