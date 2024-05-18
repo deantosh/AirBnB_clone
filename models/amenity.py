a6ddfc0a24e714f39afa72740a35402cc975eefa
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-18 22:48:14 +0300

    Amenity class added

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+Module defines `Amenity` class
+"""
+
+
+class Amenity(BaseModel):
+    """defines `Amenity` class"""
+    name = ""
