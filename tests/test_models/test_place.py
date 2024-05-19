1932e9b30de074319c787999d33bfd7a4e426d1d
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-19 23:51:49 +0300

    test: Place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Module defines a test for `Place` class
+"""
+import unittest
+from models.place import Place
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+    """Test `Place` class"""
+
+    def setUp(self):
+        """Setup test environment"""
+        self.place = Place()
+
+    def test_class_attributes(self):
+        """Test class attributes"""
+        self.assertEqual(self.place.city_id, "")
+        self.assertEqual(self.place.user_id, "")
+        self.assertEqual(self.place.name, "")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 0)
+        self.assertEqual(self.place.number_bathrooms, 0)
+        self.assertEqual(self.place.max_guest, 0)
+        self.assertEqual(self.place.price_by_night, 0)
+        self.assertEqual(self.place.latitude, 0.0)
+        self.assertEqual(self.place.longitude, 0.0)
+        self.assertEqual(self.place.amenity_ids, [])
+
+    def test_class_attributes_type(self):
+        """Test type of class attributes"""
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
+
+    def test_inherits_from_base_model(self):
+        """Test inherits from `BaseModel` class"""
+        self.assertIsInstance(self.place, BaseModel)
